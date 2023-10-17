105d7d6fd6d5a2547d632a85b3e04a7f4c344e6c
HannahAline <143466186+HannahAline@users.noreply.github.com>
2023-10-17 14:36:51 -0500

    Update ABM-Coastal-AL.R

--- ABM-Coastal-AL.R
+++ ABM-Coastal-AL.R
@@ -1,6 +1,16 @@
 #Start of model
 
-#Agents
+#Agents (Additive or multiplicative)
+
+  #Set parameters
+    #Race (15% Native American,3% Asian, 6% Black, 76% White)
+    #Gender (58% Male, 42% Female)
+    #Education (0:lower, 1:higher)
+
+  #if:then
+    #Race (0:non-white, 1:white)
+    #Gender (0:male, 1:female)
+    #Education (0:lower, 1:higher)
 
 #Environment
 
