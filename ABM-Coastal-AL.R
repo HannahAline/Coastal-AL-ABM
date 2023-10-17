823aadff210d54ce3d16648652f080d4e09add7d
HannahAline <143466186+HannahAline@users.noreply.github.com>
2023-10-17 15:06:46 -0500

    Added in code

--- ABM-Coastal-AL.R
+++ ABM-Coastal-AL.R
@@ -1,11 +1,37 @@
 #Start of model
 
+#-------------------------------------------------------------------------------
+#Initialize Population
+
+gender = unique(data$gender) 
+races = unique(data$race)
+education
+    
+      for(g in 1:length(genders)){
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      }
+
 #Agents (Additive or multiplicative)
 
-  #Set parameters
-    #Race (15% Native American,3% Asian, 6% Black, 76% White)
-    #Gender (58% Male, 42% Female)
-    #Education (0:lower, 1:higher)
+
+
+#Set parameters
+  #Race
+  #Gender 
+
+  #if:then
+    #if Race is...
+      #Then Education is...
 
   #if:then
     #Race (0:non-white, 1:white)
