37fc44e7dd570abc5127579784f5e97f89513f69
HannahAline <143466186+HannahAline@users.noreply.github.com>
2023-10-18 22:35:37 -0500

    Update ABM-Coastal-AL.R

--- ABM-Coastal-AL.R
+++ ABM-Coastal-AL.R
@@ -3,11 +3,22 @@
 #-------------------------------------------------------------------------------
 #Initialize Population
 
-gender = unique(data$gender) 
-races = unique(data$race)
+eds=unique
+
+gender = unique(data$gender) #Male = M and Female = F
+races = unique(data$race) #Native, White, Black, Asian
 education
     
       for(g in 1:length(genders)){
+        for(r in(1:length(races))){
+          pop$ed[pop$gender==gender[g] & pop$races==races[t]] = sample(
+            eds.t=data$eds[pop$gender==gender[g] & pop$races==races[t]]
+            eds.tt=table(eds.t)
+            eds.tt/sum(eds.tt)
+            
+            sample(colnames(eds.tt), probs=eds.tt, replace=T)
+          )
+        }
         
         
         
