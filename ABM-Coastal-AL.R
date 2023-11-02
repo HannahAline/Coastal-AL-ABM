cb8393bf636e805d87214534786754a523b3385b
HannahAline <143466186+HannahAline@users.noreply.github.com>
2023-11-01 20:02:38 -0500

    Update ABM-Coastal-AL.R

--- ABM-Coastal-AL.R
+++ ABM-Coastal-AL.R
@@ -4,25 +4,190 @@
 #                                                                              #
 ################################################################################
 
-#Good idea - maybe make this a model of summer tourism when there is the most boating in AL
-
-#-------------------------------------------------------------------------------
-#test
-
-# Load in the data
-
 setwd()
 
-datum = read.csv(file.choose())
+# Load required packages
+library(dplyr)
+
+# Load your demographic data from a CSV file (replace 'your_data.csv' with the actual file name)
+datum <- read.csv(file.choose())
+
 
 #-------------------------------------------------------------------------------
 
-# Notes
+#Need to change categorical variables to factors
 
-#Common terms
-  # Length = how many
-  # !is.na = 
+#Race
+datum$Race = as.factor(datum$Race)
+#Spanish
+datum$Spanish = as.factor(datum$Spanish)
+#Gender
+datum$Gender = as.factor(datum$Gender)
+#GenderBin
+datum$GenderBin = as.factor(datum$GenderBin)
+#ALResident
+datum$ALResident = as.factor(datum$ALResident)
+#Education
+datum$Education = as.factor(datum$Education)
+#EducationBin
+datum$EducationBin = as.factor(datum$EducationBin)
+#Visitor
+datum$Visitor = as.factor(datum$Visitor)
+#Beach
+datum$Beach = as.factor(datum$Beach)
+#BeachBin
+datum$BeachBin = as.factor(datum$BeachBin)
+#Turtleview
+datum$Turtleview = as.factor(datum$Turtleview)
+#TurtleviewBin
+datum$TurtleviewBin = as.factor(datum$TurtleviewBin)
+#Dolphinview
+datum$Dolphinview = as.factor(datum$Dolphinview)
+#DolphinviewBin
+datum$DolphinviewBin = as.factor(datum$DolphinviewBin)
+#Wildlifeview
+datum$Wildlifeview = as.factor(datum$Wildlifeview)
+#WildlifeviewBin
+datum$WildlifeviewBin = as.factor(datum$WildlifeviewBin)
+#Fishbeach
+datum$Fishbeach = as.factor(datum$Fishbeach)
+#FishbeachBin
+datum$FishbeachBin = as.factor(datum$FishbeachBin)
+#Fishjetty
+datum$Fishjetty = as.factor(datum$Fishjetty)
+#FishjettyBin
+datum$FishjettyBin = as.factor(datum$FishjettyBin)
+#Fishboat
+datum$Fishboat = as.factor(datum$Fishboat)
+#FishboatBin
+datum$FishboatBin = as.factor(datum$FishboatBin)
+#Boatmotor
+datum$Boatmotor = as.factor(datum$Boatmotor)
+#BoatmotorBin
+datum$BoatmotorBin = as.factor(datum$BoatmotorBin)
+#Boatsail
+datum$Boatsail = as.factor(datum$Boatsail)
+#BoatsailBin
+datum$BoatsailBin = as.factor(datum$BoatsailBin)
+#Angler
+datum$Angler = as.factor(datum$Angler)
+#Pamphlet
+datum$Pamphlet = as.factor(datum$Pamphlet)
+#Sign
+datum$Sign = as.factor(datum$Sign)
+#Magnet
+datum$Magnet = as.factor(datum$Magnet)
+#Sticker
+datum$Sticker = as.factor(datum$Sticker)
+#Bycatch
+datum$Bycatch = as.factor(datum$Bycatch)
+#Lures
+datum$Lures = as.factor(datum$Lures)
+#Hooks
+datum$Hooks = as.factor(datum$Hooks)
+#Lights
+datum$Lights = as.factor(datum$Lights)
+#Phone
+datum$Phone = as.factor(datum$Phone)
+#PolicyBin
+datum$PolicyBin = as.factor(datum$PolicyBin)
+#WildlifeBin
+datum$WildlifeBin = as.factor(datum$WildlifeBin)
 
+#-------------------------------------------------------------------------------
+
+# Calculate demographic proportions
+race_proportions <- datum %>%
+  group_by(Race) %>%
+  summarize(count = n()) %>%
+  mutate(proportion = count / sum(count))
+
+gender_proportions <- datum %>%
+  group_by(Gender) %>%
+  summarize(count = n()) %>%
+  mutate(proportion = count / sum(count))
+
+education_proportions <- datum %>%
+  group_by(Education) %>%
+  summarize(count = n()) %>%
+  mutate(proportion = count / sum(count))
+         
+         # Define the number of agents and simulation steps
+         num_agents <- 100 #This is how large the population is, and can be changed later on
+         num_steps <- 50
+         
+         #Individual based - each "agent" is a person in a coastal population
+         #  - each person/agent has properties
+         #  - parameters and attributes
+         #  - Agents/individuals to have behaviors based on those parameters
+         
+         # Create a data frame to represent agents
+         agents <- data.frame(
+           agent_id = 1:num_agents,
+           race = sample(datum$Race, num_agents, replace = TRUE, prob = race_proportions$proportion),#Assign race#
+           gender = sample(datum$Gender, num_agents, replace = TRUE, prob = gender_proportions$proportion), #Assign gender
+           education = sample(datum$Education, num_agents, replace = TRUE, prob = education_proportions$proportion), #Assign education
+           policy_knowledge = rep(0, num_agents),  # Initialize policy knowledge to zero
+           education_material = sample(c("sign", "pamphlet", "sticker", "magnet"), num_agents, replace = TRUE)
+         )
+         
+         # Define a function to model agent-education interactions
+         interact_with_education <- function(agent, step) {
+           # Simulate agent-education interactions
+           education_material <- agent$education_material
+           # You can implement specific behaviors for each material here
+           if (education_material == "sign") {
+             # Interaction with sign
+             # Update agent's knowledge based on the interaction
+             agent$policy_knowledge <- agent$policy_knowledge + 1
+           } else if (education_material == "pamphlet") {
+             # Interaction with pamphlet
+             # Update agent's knowledge based on the interaction
+             agent$policy_knowledge <- agent$policy_knowledge + 2
+           } else if (education_material == "sticker") {
+             # Interaction with sticker
+             # Update agent's knowledge based on the interaction
+             agent$policy_knowledge <- agent$policy_knowledge + 3
+           } else if (education_material == "magnet") {
+             # Interaction with magnet
+             # Update agent's knowledge based on the interaction
+             agent$policy_knowledge <- agent$policy_knowledge + 4
+           }
+           return(agent)
+         }
+         
+         # Define a function to model agent-wildlife interactions
+         interact_with_wildlife <- function(agent, step) {
+           # Simulate agent-wildlife interactions based on policy knowledge
+           if (agent$policy_knowledge == 1) {
+             # Positive interaction with wildlife
+             # You can implement specific behaviors here
+             agent$wildlife_interaction <- rnorm(1, mean = 5, sd = 2)
+           } else {
+             # Negative interaction with wildlife
+             # You can implement different behaviors here
+             agent$wildlife_interaction <- rnorm(1, mean = 2, sd = 1)
+           }
+           return(agent)
+         }
+         
+         # Run the simulation
+         for (step in 1:num_steps) {
+           for (i in 1:num_agents) {
+             # Interact with education material
+             agents[i, ] <- interact_with_education(agents[i, ], step)
+             # Interact with wildlife
+             agents[i, ] <- interact_with_wildlife(agents[i, ], step)
+           }
+         }
+         
+         # Explore the resulting data
+         summary(agents$wildlife_interaction)
+         
+         # Visualize and analyze the data using ggplot2, dplyr, or other libraries.
+         
+
+#-------------------------------------------------------------------------------
 
 #Parameterization: selecting values for model parameters
   #This is where you try to "break" your model - make sure it is following all of the rules
@@ -42,148 +207,6 @@ datum = read.csv(file.choose())
 
           #Be sure that known values are comparable to model outcomes
               #This is when we compare our survey data results with the model output
-
-
-#-------------------------------------------------------------------------------
-
-#parameters - these are our variables
-
-#Proportion of females
-propF = length(datum$GenderBin[datum$GenderBin==1])/length(datum$GenderBin[!is.na(datum$GenderBin)]) #Proportion of Females
-
-
-#Use the table function for the education levels
-
-#Individual based - each "agent" is a person in a coastal population
-#  - each person/agent has properties
-#  - parameters and attributes
-#  - Agents/individuals to have behaviors based on those parameters
-
-
-Agent1 <- data.frame(AgentNo = 1, #Each row is going to tell you about that agent
-                     Gender = "g", 
-                     Race = "r",
-                     Education = 'e',
-                     Dolphin = runif(1,0,1)) #This determines how often they interact
-                                            #with dolphins - Randomly gen variable
-
-# Create a population of Agents
-nPop1 <- 10 #This is how large the population is, and can be changed later on
-
-for( i in 2:nPop1){
-  Agent2 <- data.frame(AgentNo = i, #Each row is going to tell you about that agent
-                       Gender = "g",
-                       Race = "r",
-                       Education = 'e',
-                       Dolphin = runif(1,0,1))
-  Agent1 <- rbind( Agent1, Agent2)
-  
-} 
-
-Agent1$Gender = sample(c(0,1), replace=T, prob=c(propF, (1-propF))) #Assign gender
-
-#Determine their probability of interacting with a dolphin
-Mix1 <- Agent1$Dolphin[ i ]
-#How many agents will they meet
-Meet1 <- round(Mix1*3,0) #This allows us to see how dolphins they see
-#Grab the dolphins they will meet
-Meet2 <- sample( 1:nPop1, Meet1, replace = TRUE)
-
-for( j in 1:length(Meet2) ){
-  Agent1[ Meet2[j], ]
-}
-
-#Table provides a summary of agents that interact with a dolphin?
-table( Agent1$State)
-
-#parameters
-eds           =unique #This is our original data, which will be loaded in
-pop           = data.fram(id = seq(1,npop, 1))#create an object for pop here
-
-timeperiod    = 50 #or years
-sexratio      = 0.5 
-race          =
-#if you think you will change this later on, it is easier to put in as a parameter
-#Add in other demographics later? Maybe age, zipcode, angler, visit motivations
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#-------------------------------------------------------------------------------
-#Initialize Population
-#We need to set the population for the model and assign values to various 
-#parameters. These include gender, race, and education (which will be determined
-#based on the individuals gender and race with a nested loop)
-
-
-
-
-
-#Set the different population parameters
-  #This will be characterized from our original data
-gender = unique(data$gender)
-races = unique(data$race) 
-
-#Nested loop (set genders --> race --> education)
-education #Need help getting the loop code
-
-#This if:then nested loop means...
-#if Gender/Race is...
-#Then Education is...
-
-
-      for(g in 1:length(genders)){ #number of genders - 2
-        
-        for(r in(1:length(races))){ #number of races - 4
-          pop$ed[pop$gender==gender[g] & pop$races==races[t]] = sample( #
-            eds.t=data$eds[pop$gender==gender[g] & pop$races==races[t]]
-            eds.tt=table(eds.t)
-            eds.tt/sum(eds.tt)
-            
-            sample(colnames(eds.tt), probs=eds.tt, replace=T)
-          )
-        }
-
-      }
-
-#-------------------------------------------------------------------------------       
-#Set behaviors and model rules based on their demographics (gender, race, education)        
-
-#Set their knowledge of wildlife/policy based on their demographics
-#This is related to their willingness to change behavior
-
-#(we will get this relationship from the glm we are running)
-  #i.e. males are less likely, white is less likely, etc.
-
-#These if:then rules are based on their demographic variables
-  #Such as, IF they are more knowledgeable about wildlife based on their
-  #demographics, THEN they are more willing to change their behavior
-
-#-------------------------------------------------------------------------------       
-#Let the model run with the given rules from above
-
-#Set loop length, maybe 1 week?
-
-#Here we could also put an education component, which would change peoples behaviors
-  #Maybe a probability of encountering signage
-
-#We want to be able to change peoples ability to 'learn' and 'forget' rules
 
 #-------------------------------------------------------------------------------
 #Output
